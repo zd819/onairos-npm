2e50cf341bcdf12a8153505ab22a21427be9210d
DjDarkz <zd819@ic.ac.uk>
2024-08-18 20:56:48 +0100

    Build ES Module too

---
+++ dist/onairos.esm.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Onairos", {
+  enumerable: true,
+  get: function () {
+    return _onairos.Onairos;
+  }
+});
+var _onairos = require("./onairos");
---
+++ src/index.js
@@ -0,0 +1,6 @@
+// src/index.js
+
+import { Onairos } from "./onairos";
+export { Onairos };
+
+// You can also export additional functions or constants if needed
